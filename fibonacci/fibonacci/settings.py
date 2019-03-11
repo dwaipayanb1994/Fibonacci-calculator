cf5c909d7a84f913330d04ee75103831e59eb50f
Dwaipayan Barman <dwaipayanb1994@gmail.com>
2019-03-11 23:51:47 +0530

    final2

--- fibonacci/fibonacci/settings.py
+++ fibonacci/fibonacci/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_ROOT = STATIC_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
